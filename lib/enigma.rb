36e9b09f1083aafabf8800b3d8927c6199f3ac08
Alice Post <alicep2322@gmail.com>
2019-09-15 16:41:24 -0600

    Update enigma.encrypt method to return an encrypted message given a message, key, and date

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -6,9 +6,10 @@ class Enigma
 
   def encrypt(message, key, date)
     encryption = Encryption.new(message, key, date)
+    encrypted_message = encryption.shift_message(encryption.message_in, encryption.final_offset)
 
     encrypted_hash = {
-      encryption: encryption.encrypted_text,
+      encryption: encrypted_message,
       key: key,
       date: date,
     }
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -13,13 +13,10 @@ class EnigmaTest < Minitest::Test
   end
 
   def test_attributes_at_init
-    encryption = mock("hello world")
-    encryption.expects(:encrypted_text).returns("keder ohulw")
-    # binding.pry
     expected = {encryption: "keder ohulw",
                 key: "02715",
                 date: "040895"}
 
-    assert_equal expected, @enigma.encrypt(message, "02715", "040895")
+    assert_equal expected, @enigma.encrypt("hello world", "02715", "040895")
   end
 end
