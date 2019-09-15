8d231c59707e9828f82b2eabf449315ae6168539
Alice Post <alicep2322@gmail.com>
2019-09-15 12:44:33 -0600

    Make hash of given keys to ABCD

--- lib/encryption.rb
+++ lib/encryption.rb
@@ -5,4 +5,15 @@ class Encryption
     @date = date
   end
 
+  def keys_letters(key)
+    num_array = key.chars
+    keys_hash = {A: nil, B: nil, C: nil, D:nil}
+    keys_hash.each_with_index do |(key, value), index|
+      if index < keys_hash.length
+        value = num_array[index] + num_array[index + 1]
+        keys_hash[key] = value.to_i
+      end
+    end
+  end
+
 end
--- test/encryption_test.rb
+++ test/encryption_test.rb
@@ -2,6 +2,7 @@ require './test_helper'
 require 'date'
 require_relative '../lib/enigma'
 require_relative '../lib/encryption'
+require 'pry'
 
 class EncrypionTest < Minitest::Test
   def setup
@@ -12,6 +13,16 @@ class EncrypionTest < Minitest::Test
     assert_instance_of Encryption, @encryption
   end
 
-  def test_attributes_at_init
+  def test_keys_letters
+    num_key = "23456"
+    expected = {A: 23, B: 34, C:45, D: 56}
+    assert_equal expected, @encryption.keys_letters(num_key)
+  end
+
+  def test_offset
+    date = "091519"
+    expected = {A:7, B:3, C:6, D:1}
+
+    assert_equal expected, @encryption.offset(date)
   end
 end
