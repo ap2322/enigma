5e28719fcfe67e99585818ff0176d9da26ea7cb2
Alice Post <alicep2322@gmail.com>
2019-09-16 11:31:04 -0600

    Make Decryption class and test.

--- lib/decryption.rb
+++ lib/decryption.rb
@@ -11,30 +11,30 @@ class Decryption
     @date = date
   end
 
-  # def keys_letters(key)
-  #   num_array = key.chars
-  #   keys_hash = {A: nil, B: nil, C: nil, D:nil}
-  #   keys_hash.each_with_index do |(key, value), index|
-  #     if index < keys_hash.length
-  #       value = num_array[index] + num_array[index + 1]
-  #       keys_hash[key] = value.to_i
-  #     end
-  #   end
-  #   keys_hash
-  # end
-  #
-  # def offset(date)
-  #   last_4_of_square = (date.to_i**2).digits.reverse[-4..-1]
-  #   key_date = {A:nil, B:nil, C: nil, D: nil}
-  #   key_date.each_with_index do |(key, value), index|
-  #     key_date[key] = last_4_of_square[index]
-  #   end
-  #   key_date
-  # end
-  #
-  # def final_offset
-  #   @final_offset = keys_letters(key).merge(offset(date)) do |key, oldv, newv|
-  #     oldv + newv
-  #   end
-  # end
+  def keys_letters(key)
+    num_array = key.chars
+    keys_hash = {A: nil, B: nil, C: nil, D:nil}
+    keys_hash.each_with_index do |(key, value), index|
+      if index < keys_hash.length
+        value = num_array[index] + num_array[index + 1]
+        keys_hash[key] = value.to_i
+      end
+    end
+    keys_hash
+  end
+
+  def offset(date)
+    last_4_of_square = (date.to_i**2).digits.reverse[-4..-1]
+    key_date = {A:nil, B:nil, C: nil, D: nil}
+    key_date.each_with_index do |(key, value), index|
+      key_date[key] = last_4_of_square[index]
+    end
+    key_date
+  end
+  
+  def final_offset
+    @final_offset = keys_letters(key).merge(offset(date)) do |key, oldv, newv|
+      oldv + newv
+    end
+  end
 end
