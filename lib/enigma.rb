acc870fb5260154db6483c8f0af0454f9697e932
Alice Post <alicep2322@gmail.com>
2019-09-15 18:04:03 -0600

    Add default date value in encrypt

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -3,9 +3,10 @@ require_relative './key_date'
 
 class Enigma
   def initialize
+    @date = date
   end
 
-  def encrypt(message, key, date)
+  def encrypt(message, key, date = self.date)
     encryption = Encryption.new(message, key, date)
     encrypted_message = encryption.shift_message(encryption.message_in, encryption.final_offset)
 
@@ -17,7 +18,7 @@ class Enigma
     encrypted_hash
   end
 
-  def today
-    date = KeyDate.new.date
+  def date
+    KeyDate.new.date
   end
 end
