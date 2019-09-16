ca2ee8167c6d9f905c10b6cdb488f6d97f2eefe2
Alice Post <alicep2322@gmail.com>
2019-09-16 15:16:04 -0600

    Add file creation command line message

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -9,3 +9,8 @@ encrypted_hash = Enigma.new.encrypt(message_to_encrypt)
 writer = File.open(ARGV[1], "w")
 writer.write(encrypted_hash[:encryption])
 writer.close
+
+message = "Created #{ARGV[1]} with the key #{encrypted_hash[:key]} \
+and date #{encrypted_hash[:date]}"
+
+puts message
