48fdfad28e039838a5a490699516f6e5b46ed4e9
Alice Post <alicep2322@gmail.com>
2019-09-16 13:56:05 -0600

    Add key method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -6,23 +6,28 @@ require_relative './date'
 class Enigma
   def initialize
     @date = date
+    @key = key
   end
 
-  def encrypt(message, key, date = @date)
+  def encrypt(message, key = @key, date = @date)
     encryption = Encryption.new(message, key, date)
     encrypted_message = encryption.shift_message(
                         encryption.message_in, encryption.final_offset)
     { encryption: encrypted_message, key: key, date: date, }
   end
 
-  def decrypt(message, key, date = @date)
+  def decrypt(message, key = @key, date = @date)
     decryption = Decryption.new(message, key, date)
     decrypted_message = decryption.shift_message(
       decryption.message_in, decryption.final_offset)
-    { encryption: decrypted_message, key: key, date: date, }
+    { decryption: decrypted_message, key: key, date: date, }
   end
 
   def date
     Date.new.date
+  end
+
+  def key
+    Key.new.random_number
   end
 end
