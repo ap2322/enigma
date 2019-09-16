7c662072018ca30f4e325ac282e9c674ffdfcaa4
Alice Post <alicep2322@gmail.com>
2019-09-16 12:26:39 -0600

    Move the keys_letters hash and offset hash into the Shiftable module

--- test/decryption_test.rb
+++ test/decryption_test.rb
@@ -19,18 +19,8 @@ class DecryptionTest < Minitest::Test
     assert_equal "040895", @decryption.date
   end
 
-  def test_keys_letters
-    expected = {A: 02 , B: 27 , C: 71 , D: 15 }
-    assert_equal expected, @decryption.keys_letters("02715")
-  end
-
-  def test_offset
-    expected = {A: 1, B: 0, C: 2, D: 5}
-    assert_equal expected, @decryption.offset("040895")
-  end
-
   def test_final_offset
-    expected = {A: 3, B: 27, C: 73, D: 20}
+    expected = {A: -3, B: -27, C: -73, D: -20}
 
     assert_equal expected, @decryption.final_offset
   end
--- test/encryption_test.rb
+++ test/encryption_test.rb
@@ -13,17 +13,10 @@ class EncrypionTest < Minitest::Test
     assert_instance_of Encryption, @encryption
   end
 
-  def test_keys_letters
-    num_key = "23456"
-    expected = {A: 23, B: 34, C:45, D: 56}
-    assert_equal expected, @encryption.keys_letters(num_key)
-  end
-
-  def test_offset
-    date = "091519"
-    expected = {A:7, B:3, C:6, D:1}
-
-    assert_equal expected, @encryption.offset(date)
+  def test_attributes_at_init
+    assert_equal "Hello World", @encryption.message_in
+    assert_equal "23456", @encryption.key
+    assert_equal "091519", @encryption.date
   end
 
   def test_final_offset
