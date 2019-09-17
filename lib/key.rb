6fc404a13c890f7cf2406b8268e72fc21d5cc2dd
Alice Post <alicep2322@gmail.com>
2019-09-17 13:20:57 -0600

    Key class now uses a class method to generate a random number string in the proper format

--- lib/key.rb
+++ lib/key.rb
@@ -1,14 +1,11 @@
 class Key
 
-  def initialize
-    @number = random_number
-  end
-
-  def random_number
+  def self.random_number
     num_array = []
     5.times do
       num_array << rand(0..9)
     end
     num_array.join.to_s
   end
+  
 end
